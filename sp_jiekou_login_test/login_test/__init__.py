f7884c4f4c374b8bea550df22b82e5e27f948120
lch <18639131823@163.com>
2019-08-08 15:12:23 +0800

    Merge remote-tracking branch 'origin/master' into master2
    # Conflicts:
    #	sp_jiekou_login_test/login_test/__init__.py

--- sp_jiekou_login_test/login_test/__init__.py
+++ sp_jiekou_login_test/login_test/__init__.py
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-import requests
-import json
 
 
-def test_login4():
-    '''该测试用例针对于账号或密码输入错误'''
-    date={
-        "grant_type": "password",
-        "client_id": "295d82a74c456c89d9110cee290ab915",
-        "device_id": "web_signin",
-        "identity": "cfruit.yhg01",
-    "password": "QAt$*43r1"
-                       }
-    r=requests.post(url="https://uat-sunpeopleprx-2.scgdomain.com/usercenter/token",data=date)
-    print(type(r))
-    print(type(r.text))
-    res=json.loads(r.text)
-    print(res['error'])
-    print(type(res))
-test_login4()
-=======
 
->>>>>>> origin/master
